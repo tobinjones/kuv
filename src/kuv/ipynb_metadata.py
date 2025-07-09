4a7f8fc7a24129b44a4473742e3eddcd14294740
Tobin Jones <tobin.jones@buckleysystems.com>
2025-07-09 13:28:04 +1200

    Change to executing script rather than parsing requirements

--- src/kuv/ipynb_metadata.py
+++ src/kuv/ipynb_metadata.py
@@ -1,51 +1,37 @@
 import re
-import tomllib
+
 import nbformat
 
 REGEX = r"(?m)^# /// (?P<type>[a-zA-Z0-9-]+)$\s(?P<content>(^#(| .*)$\s)+)^# ///$"
 
-
-def read_script_metadata(source: str) -> dict | None:
+def find_script_metadata_blocks(source: str) -> list[str]:
     """Read script metadata from python source
 
     See https://peps.python.org/pep-0723/#reference-implementation
     """
     name = "script"
-    matches = list(
-        filter(lambda m: m.group("type") == name, re.finditer(REGEX, source))
-    )
-    if len(matches) > 1:
-        raise ValueError(f"Multiple {name} blocks found")
-    elif len(matches) == 1:
-        content = "".join(
-            line[2:] if line.startswith("# ") else line[1:]
-            for line in matches[0].group("content").splitlines(keepends=True)
-        )
-        return tomllib.loads(content)
-    else:
-        return None
+    matches = filter(lambda m: m.group("type") == name, re.finditer(REGEX, source))
+    return list(m.string for m in matches)
 
+def get_ipynb_script_metadata(file:str) -> str | None:
+    """Get script metadata from ipynb file
 
-def read_ipynb_script_metadata(file: str) -> dict | None:
-    """Read script metadata from ipynb file
-
-    Metadata must be in a code cell. Cannot be split across multiple cells. Only one metadata block is allowed.
+    Metadata must be in a code cell. Cannot be split across multiple cells. Only one metadata block is allowed per notebook.
     """
     with open(file, "r", encoding="utf-8") as f:
         notebook = nbformat.read(f, as_version=4)
 
     code_cells = (cell for cell in notebook.cells if cell.cell_type == "code")
+
     script_metadata_blocks = [
-        metadata
+        block
         for cell in code_cells
-        if (metadata := read_script_metadata(cell.source)) is not None
+        for block in find_script_metadata_blocks(cell.source)
     ]
 
-    if len(script_metadata_blocks) > 1:
-        raise ValueError("Multiple script blocks found")
+    if len(script_metadata_blocks) == 0:
+        return None
     elif len(script_metadata_blocks) == 1:
-        script_metadata = script_metadata_blocks[0]
+        return script_metadata_blocks[0]
     else:
-        script_metadata = None
-
-    return script_metadata
+        raise ValueError("Multiple script blocks found")
--- src/kuv/kernel_launcher.py
+++ src/kuv/kernel_launcher.py
@@ -1,50 +1,61 @@
 import argparse
+import os
 import subprocess
 import sys
-import os
-import uv
-from kuv.ipynb_metadata import read_ipynb_script_metadata
+import tempfile
 
+import uv
+
+from kuv.ipynb_metadata import get_ipynb_script_metadata
+
+ipykernel_launch_script = """
+from ipykernel import kernelapp as app
+app.launch_new_instance()
+"""
 
 def main():
+    # Get connection file from arguments
     parser = argparse.ArgumentParser(description="kuv")
-    parser.add_argument(
-        "-f", "--connection-file", required=True, help="Path to connection file"
-    )
+    parser.add_argument( "-f", "--connection-file", required=True)
     args = parser.parse_args()
 
     # Get metadata from notebook
     notebook_path = os.environ.get("JPY_SESSION_NAME", None)
-    meta = read_ipynb_script_metadata(notebook_path)
+    if notebook_path is None:
+        script_metadata = None
+    else:
+        script_metadata = get_ipynb_script_metadata(notebook_path)
+
+    # uv executable from python package
     uv_executable = uv.find_uv_bin()
-    cmd = [
-        uv_executable,
-        "run",
-        "--no-project",
-        "--isolated",
-    ]
 
-    if meta is not None:
-        if "requires-python" in meta:
-            cmd += ["--python", meta["requires-python"]]
-        if "dependencies" in meta:
-            for dependency in meta["dependencies"]:
-                cmd += ["--with", dependency]
+    with tempfile.NamedTemporaryFile("w+", suffix=".py") as f:
+        # Paste in the metadata block from the notebook
+        if script_metadata is not None:
+            f.write(script_metadata)
 
-    cmd += [
-        "--with",
-        "ipykernel",
-        "python",
-        "-m",
-        "ipykernel_launcher",
-        "-f",
-        args.connection_file,
-    ]
+        # The launch script runs ipykernel
+        f.write(ipykernel_launch_script)
+        f.flush()
 
-    proc = subprocess.Popen(cmd)
-    proc.wait()
-    sys.exit(proc.returncode)
+        # Now we execute the script using uv to
+        # create the virtualenv
+        args = [
+            uv_executable,
+            "run",
+            "--no-project",
+            "--with",
+            "ipykernel",
+            f.name,
+            "-f",
+            args.connection_file,
+        ]
+
+        result = subprocess.run(args)
+
+    sys.exit(result.returncode)
+
 
 # This is executable as a module, e.g. python -m kuv.kernel_launcher
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
